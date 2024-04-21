56f0774a751c222562e62c7886865e7cd4ad0738
Ramiro <ramiro.depaoli@gmail.com>
2024-04-21 13:48:37 -0300

    better response

--- src/main/java/com/ramide1/mcgpt/Gpt.java
+++ src/main/java/com/ramide1/mcgpt/Gpt.java
@@ -62,8 +62,7 @@ public class Gpt implements CommandExecutor {
                     Pattern pattern = Pattern.compile(regex);
                     Matcher matcher = pattern.matcher(response.toString());
                     if (matcher.find()) {
-                        content = matcher.group(1);
-                        content = content.replace("\n", " ");
+                        content = matcher.group(1).replace("\n", " ").replace("\n\n", " ");
                         newHistory = newHistory + "," + "{\"role\": \"assistant\",\"content\": \"" + content + "\"}";
                         saveHistory(sender, newHistory);
                     }
